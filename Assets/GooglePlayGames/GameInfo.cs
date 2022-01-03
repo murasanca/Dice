6909ee049eadc7cf156a2fabd397547918ad59d7
Murat Sancak <murasanca@protonmail.com>
2022-01-03 18:09:39 +0300

    2022.1.3

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "1024559272214"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = "1024559272214-ccd9kd4e73i074adpcl06o34cotdt9gl.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = ""; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
